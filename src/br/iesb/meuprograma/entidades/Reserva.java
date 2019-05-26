2fba5bd788b5e672da1af8abb69499a0081c0d7b
tuliomonteiro <tuliomonteiro.s@gmail.com>
2019-05-26 16:47:09 -0300

    adicionando entidade, DAO, e BO de Reserva

---
+++ src/br/iesb/meuprograma/entidades/Reserva.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.iesb.meuprograma.entidades;
+
+/**
+ *
+ * @author Tulio Monteiro
+ */
+public class Reserva {
+
+    private Integer id;
+    private String apartamento;
+    private String condomino;
+    private String cpf;
+    private String nome;
+    private String rg;
+    private String dataNascimento;
+    private String sexo;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getApartamento() {
+        return apartamento;
+    }
+
+    public void setApartamento(String apartamento) {
+        this.apartamento = apartamento;
+    }
+
+    public String getCondomino() {
+        return condomino;
+    }
+
+    public void setCondomino(String condomino) {
+        this.condomino = condomino;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String datasNascimento) {
+        this.dataNascimento = datasNascimento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+}
